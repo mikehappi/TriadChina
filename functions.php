4d7c8bda67265ad0360fdb4314309965d07436d4
Mike HAPPI <mikehappi@me.com>
2020-03-07 16:55:27 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -163,6 +163,6 @@ if ( defined( 'JETPACK__VERSION' ) ) {
  * Register Custom Navigation Walker
  */
 function register_navwalker(){
-	require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+	require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
 }
 add_action( 'after_setup_theme', 'register_navwalker' );
