539e80188639f7b56d6e08253084755e8c755805
Mike HAPPI <mikehappi@me.com>
2020-03-07 19:56:14 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'triadchina_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'triadchina' ),
+			'primary' => __( 'Primary Menu', 'triadchina' ),
 		) );
 
 		/*
@@ -157,4 +157,13 @@ require get_template_directory() . '/inc/customizer.php';
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
+}
+
+
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+    require_once get_template_directory . '/class-wp-bootstrap-navwalker.php';
 }
