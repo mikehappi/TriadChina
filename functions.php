2d93fd1003042fea69378aeee10b5161173b153f
Mike HAPPI <mikehappi@me.com>
2020-03-07 18:25:21 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -120,6 +120,8 @@ add_action( 'widgets_init', 'triadchina_widgets_init' );
  * Enqueue scripts and styles.
  */
 function triadchina_scripts() {
+	wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css");
+	
 	wp_enqueue_style( 'triadchina-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'triadchina-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
