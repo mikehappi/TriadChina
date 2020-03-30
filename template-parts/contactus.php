e81d5d4aed6f31ebbed70b254aaab38ed328907e
Mike HAPPI <mikehappi@me.com>
2020-03-30 15:16:35 +0800

    Update contactus.php

--- template-parts/contactus.php
+++ template-parts/contactus.php
@@ -90,6 +90,19 @@ background-color: #F2F2F2;
   height: 333px !important;
 
 }
+
+.overlay{
+  position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 0;
+    width: 592px !important;
+    height: 333px !important;
+}
  .center {
   position: absolute;
   top: 50%;
