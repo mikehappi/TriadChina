7f83fe484174ff46e9de65e528ec218b60cf039b
Mike HAPPI <mikehappi@me.com>
2020-04-03 16:25:48 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -98,17 +98,3 @@ get_header(); ?>
 ?>
 
 </div>
-<script>
-/*
-	Smooth scroll functionality for anchor links (animates the scroll
-	rather than a sudden jump in the page)
-*/
-$('.navitem').click(function(e){
-  e.preventDefault();
-  var target = $($(this).attr('href'));
-  if(target.length){
-    var scrollTo = target.offset().top;
-    $('body, html').animate({scrollTop: scrollTo+'px'}, 1000);
-  }
-});
-</script>
