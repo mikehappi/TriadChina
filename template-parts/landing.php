1e721f4232e25e7e27e441ae7f858ceadd28f1b2
Mike HAPPI <mikehappi@me.com>
2020-04-03 16:14:06 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -98,19 +98,3 @@ get_header(); ?>
 ?>
 
 </div>
-
-<script>$(".navitem").click(function(e){
-  e.preventDefault();
-
-  var this_offset = $(this).offset();
-  var that_id     = $(this).attr("href");
-  var that_offset = $(that_id).offset();
-  var offset_diff = Math.abs(that_offset.top - this_offset.top);
-
-  var base_speed  = 100; // Time in ms per 1,000 pixels
-  var speed       = (offset_diff * base_speed) / 1000;
-
-  $("html,body").animate({
-    scrollTop: that_offset.top
-  }, speed);
-});
