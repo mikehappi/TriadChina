60569cffac4561dc24234c53bbf61b5720e68748
Mike HAPPI <mikehappi@me.com>
2020-04-07 12:32:51 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -98,7 +98,6 @@ get_header(); ?>
 ?>
 
 </div>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="<?php bloginfo('template_url'); ?>/js/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
