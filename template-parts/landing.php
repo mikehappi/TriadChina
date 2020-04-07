620fae7b0e21300ed717d85d89be75d8dfac8bec
Mike HAPPI <mikehappi@me.com>
2020-04-07 12:33:14 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -113,10 +113,10 @@ $(document).ready(function(){
       var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (1000) specifies the number of milliseconds it takes to scroll to the specified area
+      // The optional number (1200) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1000, function(){
+      }, 1200, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
