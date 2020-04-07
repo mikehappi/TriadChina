7b9aa770143e80500e1bda6175d99fb4ac581f2b
Mike HAPPI <mikehappi@me.com>
2020-04-07 11:26:23 +0800

    Update landing.php

--- template-parts/landing.php
+++ template-parts/landing.php
@@ -98,5 +98,3 @@ get_header(); ?>
 ?>
 
 </div>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/js/bootstrap.min.js'></script>
