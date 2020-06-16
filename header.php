194d551ea0e24f162d4372d13974697fe06ef7dd
Mike HAPPI <mikehappi@me.com>
2020-06-16 12:44:06 +0800

    Update header.php

--- header.php
+++ header.php
@@ -95,7 +95,7 @@ var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("nav").style.top = "0";
   } else {
-    document.getElementById("nav").style.top = "-55px";
+    document.getElementById("nav").style.top = "-135px";
   }
   prevScrollpos = currentScrollPos;
 }
